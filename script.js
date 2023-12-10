824b38107ad7fa8df1d4b1a23b35e9de97e02892
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-12-10 22:50:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function stringChop(str, size) {
   // your code here
+	 // Check for null or empty string
+  if (str === null || str === '') {
+    return [];
+  }
+
+  // Initialize the result array
+  const result = [];
+
+  // Loop through the string in chunks of the specified length
+  for (let i = 0; i < str.length; i += size) {
+    // Extract a chunk of the specified length
+    const chunk = str.substr(i, size);
+
+    // Add the chunk to the result array
+    result.push(chunk);
+  }
+
+  return result;
 }
 
 // Do not change the code below
